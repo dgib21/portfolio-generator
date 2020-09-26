7a947df78ada323ffcdf522c954a2af3998cc04d
Devin Gibson <dgib21@hotmail.com>
2020-09-26 13:49:29 -0500

    added functionaity

--- app.js
+++ app.js
@@ -1,75 +1,99 @@
-// var profileDataArgs = process.argv.slice(2, process.argv.length);
-// //console.log(profileDataArgs);
-
-// // Notice the lack of parentheses around the `profileDataArr` parameter?
-// const printProfileData = profileDataArr => {
-//      for (let i = 0; i < profileDataArr.length; i += 1) {
-//        console.log(profileDataArr[i]);
-//      }
-//    };
+const inquirer = require('inquirer');
 
 
-//   printProfileData(profileDataArgs);
-  
-// // Using new arrow function syntax
-// const addNums = (numOne, numTwo) => {
-//     return numOne + numTwo;
-//   };
+// const fs = require('fs');
+// const generatePage = require('./src/page-template');
 
-//   // var is function-scoped, so redeclaring it in a block will cause its value outside the block to change as well:
+// const pageHTML = generatePage(name, github);
 
-// var one = 'one: declared outside the block';
+// fs.writeFile('./index.html', pageHTML, err => {
+//   if (err) throw err;
 
-// if (true === true) {
-//   var one = 'one: declared inside the block'; // notice: we redeclare 'one' here
-//   console.log(one); // prints 'one: declared inside the block'
-// }
+//   console.log('Portfolio complete! Check out index.html to see the output!');
+// });
 
-// console.log(one); // also prints 'one: declared inside the block', because the variable was redeclared in the 'if' block. The outer 'var' variable was therefore destroyed and replaced by inner var variable.
+// console.log(inquirer); 
 
-// // 'let' is block-scoped, so redeclaring a 'let' variable inside of a block creates a different 'let' variable with the same name whose scope is inside the block:
+//prompt user function 
+const promptUser = () => {
+  return inquirer.prompt([
+    {
+      type: 'input',
+      name: 'name',
+      message: 'What is your name?'
+    },
+    {
+      type: 'input',
+      name: 'github',
+      message: 'Enter your GitHub Username'
+    },
+    {
+      type: 'input',
+      name: 'about',
+      message: 'Provide some information about yourself:'
+    }
+  ]);
+};
 
-// let two = 'two: declared outside the block';
+const promptProject = portfolioData => {
+// If there's no 'projects' array property, create one
+if (!portfolioData.projects) {
+  portfolioData.projects = [];
+}
+  console.log(`
+=================
+Add a New Project
+=================
+`);
+  return inquirer.prompt([
+    {
+      type: 'input',
+      name: 'name',
+      message: 'What is the name of your project?'
+    },
+    {
+      type: 'input',
+      name: 'description',
+      message: 'Provide a description of the project (Required)'
+    },
+    {
+      type: 'checkbox',
+      name: 'languages',
+      message: 'What did you build this project with? (Check all that apply)',
+      choices: ['JavaScript', 'HTML', 'CSS', 'ES6', 'jQuery', 'Bootstrap', 'Node']
+    },
+    {
+      type: 'input',
+      name: 'link',
+      message: 'Enter the GitHub link to your project. (Required)'
+    },
+    {
+      type: 'confirm',
+      name: 'feature',
+      message: 'Would you like to feature this project?',
+      default: false
+    },
+    {
+      type: 'confirm',
+      name: 'confirmAddProject',
+      message: 'Would you like to enter another project?',
+      default: false
+    }
+  ])
+  .then(projectData => {
+    portfolioData.projects.push(projectData);
+    if (projectData.confirmAddProject) {
+      return promptProject(portfolioData);
+    } else {
+      return portfolioData;
+    }
+  });
+};
 
-// if (true === true) {
-//   let two = 'two: declared inside the block';
-//   console.log(two); // prints 'two: declared inside the block'
-// }
-
-// console.log(two); // prints 'two: declared outside the block', because two declared inside the block is a separate variable. The 'let' variables are unrelated and therefore are unaffected by each other.
-
-//const generatePage = (userName, githubName) => `Name: ${userName}, Github: ${githubName}`;
-
-const fs = require('fs');
-
-const profileDataArgs = process.argv.slice(2);
-
-const [name, github] = profileDataArgs;
-//allows us to use the generate page function housed in the page-template file
-const generatePage = require('./src/page-template.js');
 
 
-// const generatePage = (name, github) => {
-//   return `
-//   <!DOCTYPE html> 
-//   <html lang="en"> 
-//   <head>
-//     <meta charset="UTF-8">
-//     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-//     <meta http-equiv="X-UA-Compatible" content="ie=edge">
-//     <title>Portfolio Demo</title>
-//   </head>
-
-//   <body>
-//     <h1>${name}</h1>
-//     <h2><a href="https://github.com/${github}">Github</a></h2>
-//   </body>
-//   </html>
-//   `;
-// };
-
-fs.writeFile('./index.html', generatePage(name, github), err => {
-  if (err) throw new Error(err);
-
-  console.log('Portfolio complete! Check out index.html to see the output!');
-});
+promptUser()
+  .then(promptProject)
+  .then(portfolioData => {
+    console.log(portfolioData);
+  });
